6cdb6f4aacc05ef47a90044fbae683645ae3faf1
Mark Romphf <markromphf@gmail.com>
2017-10-06 16:40:50 -0600

    Favor four-space tabs over two.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function() {
-  var cells = document.getElementsByTagName("td");
-  for (var i = 0; i < cells.length; i++ ) {
-    cells[i].title = "Click to copy";
-    cells[i].addEventListener("click", function(event) {
-      var text = document.getElementsByTagName("textarea")[0];
-      text.innerHTML = event.target.innerHTML;
-      text.select();
-      document.execCommand('copy');
-    });
-  };
+    var cells = document.getElementsByTagName("td");
+    for (var i = 0; i < cells.length; i++ ) {
+        cells[i].title = "Click to copy";
+        cells[i].addEventListener("click", function(event) {
+            var text = document.getElementsByTagName("textarea")[0];
+            text.innerHTML = event.target.innerHTML;
+            text.select();
+            document.execCommand('copy');
+        });
+    };
 })();
